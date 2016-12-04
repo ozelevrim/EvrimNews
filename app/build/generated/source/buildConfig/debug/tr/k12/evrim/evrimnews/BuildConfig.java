e161c1e3f23405037739ca12d95f026d41a0b86f
TurboProgramming <ardacebi1@gmail.com>
2016-12-04 22:23:45 +0300

    Admin Panel Files added
    Added new files that include the brand new designed Admin Panel for the
    Admin and developer team to create creative content for their audience.

---
+++ app/build/generated/source/buildConfig/debug/tr/k12/evrim/evrimnews/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tr.k12.evrim.evrimnews;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tr.k12.evrim.evrimnews";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
