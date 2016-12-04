1a9a123a22b801b1505f6a3b940a23b916a5355e
TurboProgramming <ardacebi1@gmail.com>
2016-12-04 16:10:57 +0300

    Uploaded files

---
+++ app/src/main/java/tr/k12/evrim/evrimnews/AdminActivity.java
@@ -0,0 +1,18 @@
+package tr.k12.evrim.evrimnews;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by sedat on 3.12.2016.
+ */
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiy_admin);
+    }
+}
