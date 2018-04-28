ea33cd8c026c9e96e92da7b664884cfbe04cfe13
Arda Çebi <ardacebi1@gmail.com>
2018-04-28 19:56:22 +0300

    UI Fix

--- app/src/main/java/tr/k12/evrim/evrimnews/Main2Activity.java
+++ app/src/main/java/tr/k12/evrim/evrimnews/Main2Activity.java
@@ -651,6 +651,8 @@ public class Main2Activity extends AppCompatActivity
 
 
 
+
+
             DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
             drawer.closeDrawer(GravityCompat.START);
             return true;
